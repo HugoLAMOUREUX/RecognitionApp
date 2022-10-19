741e92ffcac3c60c8765d8d8f839c3c4371db147
Hugo LAMOUREUX <hugo.lamoureux18@gmail.com>
2022-10-19 15:38:32 +0200

    modif affichage historique

--- lib/widgets/singleRecordingItem.dart
+++ lib/widgets/singleRecordingItem.dart
@@ -5,9 +5,14 @@ import 'package:recognition/widgets/smallText.dart';
 import 'package:recognition/widgets/timeSerieChartWidget.dart';
 
 class SingleRecordingItem extends StatelessWidget {
-
-
-  const SingleRecordingItem( {Key? key, required this.owner, required this.activity,required this.date, required this.duration, required this.timeSerie}) : super(key: key);
+  const SingleRecordingItem(
+      {Key? key,
+      required this.owner,
+      required this.activity,
+      required this.date,
+      required this.duration,
+      required this.timeSerie})
+      : super(key: key);
 
   final String owner;
   final String activity;
@@ -18,58 +23,58 @@ class SingleRecordingItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 150,
-      margin: const EdgeInsets.only(left: 24,right: 24,top: 10,bottom: 10),
-      decoration: const BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.all(Radius.circular(10)),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black12,
-              offset: Offset(
-                0.0,
-                0.0,
-              ),
-              blurRadius: 5.0,
-              spreadRadius: 1.0,)
-          ]
-
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Expanded(
-            flex: 3,
-            child: Container(
-              padding: EdgeInsets.all(6),
-              decoration: BoxDecoration(
-                border: Border(
-                  bottom: BorderSide(color:Colors.black12,width: 1.0)
-                )
-              ),
-              //color: Colors.red,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  SmallText(text:"duree : ${duration}s"),
-                  SmallText(text:"date : ${date}",maxLength: 23),
-                  SmallText(text:"activity : ${activity}",maxLength: 19,),
-                ],
+        height: 150,
+        margin: const EdgeInsets.only(left: 24, right: 24, top: 10, bottom: 10),
+        decoration: const BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(10)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                offset: Offset(
+                  0.0,
+                  0.0,
+                ),
+                blurRadius: 5.0,
+                spreadRadius: 1.0,
+              )
+            ]),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Expanded(
+              flex: 3,
+              child: Container(
+                padding: EdgeInsets.all(6),
+                decoration: BoxDecoration(
+                    border: Border(
+                        bottom: BorderSide(color: Colors.black12, width: 1.0))),
+                //color: Colors.red,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SmallText(text: "duree : ${duration}s"),
+                    SmallText(text: "date : ${date}", maxLength: 17),
+                    SmallText(
+                      text: "activity : ${activity}",
+                      maxLength: 15,
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-          Expanded(
-            flex: 7,
-            child: Container(
-              //color: Colors.white,
-              margin: EdgeInsets.all(10),
-              child: Center(
-                child: TimeSeriesChartWidget(seriesdata:timeSerie,color:Colors.blueAccent),
+            Expanded(
+              flex: 7,
+              child: Container(
+                //color: Colors.white,
+                margin: EdgeInsets.all(10),
+                child: Center(
+                  child: TimeSeriesChartWidget(
+                      seriesdata: timeSerie, color: Colors.blueAccent),
+                ),
               ),
-
-            ),
-          )
-        ],
-      ));
+            )
+          ],
+        ));
   }
 }
--- lib/widgets/userHistoryWidget.dart
+++ lib/widgets/userHistoryWidget.dart
@@ -15,53 +15,57 @@ class UserHistoryWidget extends StatefulWidget {
 }
 
 class _UserHistoryWidgetState extends State<UserHistoryWidget> {
-
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-        height: 600,
+    return Expanded(
         // decoration: BoxDecoration(color: Colors.red),
         child: StreamBuilder(
-          stream: widget._firebaseFirestore.collection("timeSeries").where("Owner",isEqualTo: "rGKoKryATbft1dbNr48VP89NGyt1").snapshots(),
-          builder: (BuildContext context,AsyncSnapshot<QuerySnapshot> snapshot){
-            if(!snapshot.hasData){
-              return Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            if(snapshot.data!.docs.isEmpty){
-              return Center(
-                child: Column(
-                    children:[
-                      Icon(Icons.hourglass_empty_outlined,size: 40,),
-                      Text("No Reccordings to display")
-                    ]),
-              );
-            }
-            return ListView(
-              children: snapshot.data!.docs.map((document){
-                List<TimeDataModel> timeSerieRecording=document["TimeSerie"].map<TimeDataModel>((data) {
-                  return TimeDataModel.withAll(
-                      t: data['t'],
-                      ax: data['ax'],
-                      ay: data['ay'],
-                      az: data['az'],
-                      gx: data['gx'],
-                      gy: data['gy'],
-                      gz: data['gz']
-                  );
-                }
-                ).toList();
-                return Center(
-
-                  child: SingleRecordingItem(owner:document["Owner"],activity:document["Activity"],date:(document["Date"] as Timestamp).toDate().toString(),duration: document["Duration"],timeSerie:timeSerieRecording),
-                );
-              }).toList(),
+      stream: widget._firebaseFirestore
+          .collection("timeSeries")
+          .where("Owner", isEqualTo: "rGKoKryATbft1dbNr48VP89NGyt1")
+          .snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (snapshot.data!.docs.isEmpty) {
+          return Center(
+            child: Column(children: [
+              Icon(
+                Icons.hourglass_empty_outlined,
+                size: 40,
+              ),
+              Text("No Reccordings to display")
+            ]),
+          );
+        }
+        return ListView(
+          children: snapshot.data!.docs.map((document) {
+            List<TimeDataModel> timeSerieRecording =
+                document["TimeSerie"].map<TimeDataModel>((data) {
+              return TimeDataModel.withAll(
+                  t: data['t'],
+                  ax: data['ax'],
+                  ay: data['ay'],
+                  az: data['az'],
+                  gx: data['gx'],
+                  gy: data['gy'],
+                  gz: data['gz']);
+            }).toList();
+            return Center(
+              child: SingleRecordingItem(
+                  owner: document["Owner"],
+                  activity: document["Activity"],
+                  date: (document["Date"] as Timestamp).toDate().toString(),
+                  duration: document["Duration"],
+                  timeSerie: timeSerieRecording),
             );
-          },
-        )
-    );
+          }).toList(),
+        );
+      },
+    ));
   }
 }
 
