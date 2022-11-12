729066eba8e5ce436fd2683e865bc1833f853aab
Hugo LAMOUREUX <hugo.lamoureux18@gmail.com>
2022-11-12 19:36:12 +0100

    corrections orthographe

--- lib/screens/Guest/RegisterScreen.dart
+++ lib/screens/Guest/RegisterScreen.dart
@@ -158,25 +158,8 @@ class _RegisterScreenState extends State<RegisterScreen> {
                         ),
                       ),
                     ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        TextButton(
-                          onPressed: () {
-                            print('Forgotted Password!');
-                          },
-                          child: Text(
-                            'Forgot Password?',
-                            style: TextStyle(
-                              color: Colors.black.withOpacity(0.4),
-                              fontSize: 12.0,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
                     const SizedBox(
-                      height: 15,
+                      height: 20,
                     ),
                     ElevatedButton(
                       onPressed: () {
